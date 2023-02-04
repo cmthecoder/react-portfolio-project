5840c90d1ca7899c5a69df5ad2458c93007a45eb
Chris Mems <christopher.mems@.com>
2023-02-04 11:19:41 -0600

    hide mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,17 +18,17 @@ function NavBar() {
         </ul>
 
       {/* Hamburger */}
-      <div className='hidden'>
+      <div className='md:hidden z-10'>
         <FaBars />
       </div>
 
       {/* Mobile menu */}
-      <ul className='hidden'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+      <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+        <li className='py-6 text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Skills</li>
+        <li className='py-6 text-4xl'>Work</li>
+        <li className='py-6 text-4xl'>Contact</li>
       </ul>
       {/* Social Icons */}
       <div className='hidden'>
