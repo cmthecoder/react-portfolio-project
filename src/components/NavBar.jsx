a4ee01176a81e2c476bc9dadc500c86687d29980
Chris Mems <christopher.mems@.com>
2023-02-04 11:28:46 -0600

    make hamburger icon to toggle between mobile  menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import Logo from '../assets/CM (1).png'
 
 function NavBar() {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
@@ -18,12 +22,12 @@ function NavBar() {
         </ul>
 
       {/* Hamburger */}
-      <div className='md:hidden z-10'>
+      <div onClick={handleClick} className='md:hidden z-10'>
         <FaBars />
       </div>
 
       {/* Mobile menu */}
-      <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
         <li className='py-6 text-4xl'>Home</li>
         <li className='py-6 text-4xl'>About</li>
         <li className='py-6 text-4xl'>Skills</li>
