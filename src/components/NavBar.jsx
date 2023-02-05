556173842c00b05e148e29203f796094edb94727
Chris Mems <christopher.mems@.com>
2023-02-04 21:45:49 -0600

    implement smooth scrool for mobile view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,27 +18,27 @@ function NavBar() {
         {/* Menu */}
         <ul className='hidden md:flex'>
           <li>
-            <Link to="home" smooth={true} offset={50} duration={500}>
+            <Link to="home" smooth={true} duration={500}>
               Home
             </Link>
           </li>
           <li>
-            <Link to="about" smooth={true} offset={50} duration={500}>
+            <Link to="about" smooth={true} duration={500}>
               About
             </Link>
           </li>
           <li>
-            <Link to="skills" smooth={true} offset={50} duration={500}>
+            <Link to="skills" smooth={true} duration={500}>
               Skills
             </Link>
           </li>
           <li>
-            <Link to="work" smooth={true} offset={50} duration={500}>
+            <Link to="work" smooth={true} duration={500}>
               Work
             </Link>
           </li>
           <li>
-            <Link to="contact" smooth={true} offset={50} duration={500}>
+            <Link to="contact" smooth={true} duration={500}>
               Contact
             </Link>
           </li>
@@ -51,11 +51,31 @@ function NavBar() {
 
       {/* Mobile menu */}
       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Skills</li>
-        <li className='py-6 text-4xl'>Work</li>
-        <li className='py-6 text-4xl'>Contact</li>
+        <li className='py-6 text-4xl'>
+            <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
       </ul>
       {/* Social Icons */}
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
