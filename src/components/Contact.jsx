2faafdd76eec2ebb67dc5ddaf33f6209370d8cf4
Chris Mems <christopher.mems@.com>
2023-02-04 21:16:20 -0600

    create form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,6 +8,10 @@ function Contact() {
           <p className='text-4xl font-bold inline border-b-4 border-green-600 text-gray-300'>Contact</p>
           <p className='text-gray-300 py-4'>Submit the form below or send me an email - christopher.mems@gmail.com</p>
         </div>
+        <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+        <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+        <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+        <button className='text-white border-2 hover:bg-green-600 hover:border-green-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Connect</button>
       </form>
     </div>
   )
