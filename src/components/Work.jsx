437831c67dfd4b6f6421f30771ccf62f8e87603c
Chris Mems <christopher.mems@.com>
2023-02-04 20:46:41 -0600

    style work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import TTT from '../assets/ttt.jpg'
+import MyProps from '../assets/myprops.jpg'
 
 function Work() {
   return (
-    <div>Work</div>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-green-600'>Work</p>
+          <p className='py-6'>Checkout some of my work</p>
+        </div>
+
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          <div style={{backgroundImage: `url(${TTT})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Tic-Tac-Toe
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>App</button>
+                </a>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
