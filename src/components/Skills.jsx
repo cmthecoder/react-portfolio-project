d8dd0ff86f8786cab7c59bd35908e6c5ac4bdbb3
Chris Mems <christopher.mems@.com>
2023-02-16 20:31:48 -0600

    justify about text

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,54 +21,54 @@ function Skills() {
         <div>
           <p className='text-4xl font-bold inline border-b-4 text-[white] border-black'>Skills</p>
         </div>
-        <div className='w-full grid grid-cols-3 sm:grid-cols-4 text-center py-8'>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+        <div className='w-full grid grid-cols-3 sm:grid-cols-4 text-center py-8 text-lg'>
+          <div className='hover:scale-110 duration-500'>
             <img src={HTML} alt="HTML icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>HTML</p>
+            <p className='my-4'><strong>HTML</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={CSS} alt="CSS icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>CSS</p>
+            <p className='my-4'><strong>CSS</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={JavaScript} alt="JavaScript icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>JavaScript</p>
+            <p className='my-4'><strong>JavaScript</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={Reactphoto} alt="React icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>React</p>
+            <p className='my-4'><strong>React</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={Python} alt="Python icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>Python</p>
+            <p className='my-4'><strong>Python</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={Postgresql} alt="Postgresql icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>PostgreSQL</p>
+            <p className='my-4'><strong>PostgreSQL</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={Django} alt="Django icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>Django</p>
+            <p className='my-4'><strong>Django</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={Docker} alt="Docker icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>Docker</p>
+            <p className='my-4'><strong>Docker</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={Express} alt="Express icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>Express</p>
+            <p className='my-4'><strong>Express</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={GitHub} alt="GitHub icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>GitHub</p>
+            <p className='my-4'><strong>GitHub</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={MongoDB} alt="MongoDB icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>MongoDB</p>
+            <p className='my-4'><strong>MongoDB</strong></p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          <div className='hover:scale-110 duration-500'>
             <img src={Node} alt="Node icon" className='w-20 mx-auto pt-6'/>
-            <p className='my-4'>Node</p>
+            <p className='my-4'><strong>Node</strong></p>
           </div>
         </div>
       </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,7 +11,7 @@ function Work() {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#D0E1F9]'>Work</p>
-          <p className='py-6'>Checkout some of my work</p>
+          <p className='py-6'>Check out some of my work</p>
         </div>
 
         {/* Container */}
